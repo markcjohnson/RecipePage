6a98138b705e83e6fa31ed4aaaa4fd82c5ae8f50
Mark Johnson <markcjohnson1496@gmail.com>
2024-04-28 19:28:33 -0700

    Added credits

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,14 @@ import "./App.css";
 import RecipeCard from "./components/recipeCard/recipeCard.tsx";
 
 function App() {
-  return <RecipeCard />;
+  return (
+    <>
+      <RecipeCard />
+      <span className="author">
+        Challenge by Frontend Mentor. Coded by Mark Johnson.
+      </span>
+    </>
+  );
 }
 
 export default App;
